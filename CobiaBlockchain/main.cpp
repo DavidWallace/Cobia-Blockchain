64247017f1e3636bd8973adc43d7d55123628cf2
macbook <macbook@macbooks-MacBook-Pro.local>
2017-09-25 21:50:42 +0100

    Initial Commit

---
+++ CobiaBlockchain/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CobiaBlockchain
+//
+//  Created by macbook on 25/09/2017.
+//  Copyright © 2017 David Wallace. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
